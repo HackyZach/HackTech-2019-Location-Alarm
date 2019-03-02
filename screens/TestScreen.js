f5ff48a1759650b32eddb6b53ef491c6c6cf5cd4
Nate R <nate.ruppert@csu.fullerton.edu>
2019-03-02 11:55:24 -0800

    more demo of dialogue in test-screen

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,38 +1,45 @@
 import React, { Component } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import {
+  Text, TouchableOpacity, View, StyleSheet,
+} from 'react-native';
 import Dialog from 'react-native-dialog';
 
 export default class DialogTester extends Component {
   state = {
-    dialogVisible: false,
+    dialogOne: true,
   };
 
   showDialog = () => {
-    this.setState({ dialogVisible: true });
+    this.setState({ dialogOne: true });
   };
 
   handleCancel = () => {
-    this.setState({ dialogVisible: false });
+    this.setState({ dialogOne: false });
   };
 
   handleDelete = () => {
     // The user has pressed the "Delete" button, so here you can do your own logic.
     // ...Your logic
-    this.setState({ dialogVisible: false });
+    this.setState({ dialogOne: false });
   };
 
   render() {
-    const { dialogVisible } = this.state;
+    const { dialogOne } = this.state;
     return (
       <View>
         <TouchableOpacity onPress={this.showDialog}>
           <Text>Show Dialog</Text>
         </TouchableOpacity>
-        <Dialog.Container visible={dialogVisible}>
+        <Dialog.Container visible={dialogOne}>
           <Dialog.Title>Go commit heart stop?</Dialog.Title>
           <Dialog.Description>
             You will die immediately by this action.
           </Dialog.Description>
+          <View>
+            <Dialog.Input label="Is the color any different" />
+            <Text> OK </Text>
+            <Dialog.Input label="The answer is no haha" />
+          </View>
           <Dialog.Button label="Yesn't" onPress={this.handleCancel} />
           <Dialog.Button label="Yes" onPress={this.handleDelete} />
         </Dialog.Container>
@@ -40,3 +47,7 @@ export default class DialogTester extends Component {
     );
   }
 }
+
+// TODO Fill out
+const styles = StyleSheet.create({
+});
