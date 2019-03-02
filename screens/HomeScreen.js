e005fb35e1c5d12aec314b516728b9656c65ea25
Nate R <nate.ruppert@csu.fullerton.edu>
2019-03-02 02:22:56 -0800

    Home Screen Update to a demo-ish state

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,7 +14,7 @@ import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
-      title: 'Home Screen Test',
+      title: 'Default Dev Load Screen',
     };
 
   render() {
@@ -35,31 +35,34 @@ export default class HomeScreen extends React.Component {
           <View style={styles.getStartedContainer}>
             {this._maybeRenderDevelopmentModeWarning()}
 
-            <Text style={styles.getStartedText}>OwO?</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
+            <Text style={styles.getStartedTitle}>Welcome to our HackTech Location Based Alarm Application</Text>
 
             <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
+              A location-based alarm that triggers when a user enters a specified location. This idea was derived from taking the train and bus to CSUF where passengers are often seen sleeping or panic about missing their stop. Bicyclists can use it to pedal hard knowing their alarm will go off at the place theyre meeting their bicyclists friends. It can also serve as a reminder to drivers want to pick up something from a store.
+
+              </Text>
           </View>
 
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
+        <Text style={styles.getStartedTitle}>{"\n"}Developer Test Text</Text>
+
+        <Text style={styles.getStartedText}>
+            Note: Add app padding/Learn how to use Tab breaks in React
+            {"\n"}
+        </Text>
+
+        <Text style={styles.getStartedStuff}>
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie erat at risus facilisis porttitor. Quisque et nibh arcu. Ut auctor finibus feugiat. Nam porttitor eu metus sit amet luctus. Aliquam erat volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse rutrum neque dui, id dictum nunc varius quis. Etiam turpis magna, laoreet gravida quam vitae, sodales volutpat metus.
+        {"\n"}
+        Ut ut enim ante. Praesent id justo augue. Quisque eget lorem suscipit, tincidunt augue eu, maximus augue. Vestibulum rhoncus eget erat vel tristique. Mauris cursus augue massa, blandit euismod nunc dignissim ut. Donec viverra eros id massa laoreet auctor nec id nunc. Integer dignissim malesuada diam, ac egestas nibh tempus vitae. Sed et accumsan ante, nec blandit ante. Sed eget laoreet elit, eget sagittis orci.
+        {"\n"}
+        Phasellus convallis tristique purus, id porttitor urna interdum quis. Curabitur elementum elementum diam et ullamcorper. Curabitur ornare justo eget semper congue. Nulla tincidunt scelerisque dolor, nec porta leo fermentum a. In at dui nec tellus vulputate aliquam quis consectetur dolor. Fusce sed nibh at ante consectetur ornare in id justo. Fusce ut ullamcorper ex. Nunc ut sem rutrum, fringilla tellus at, blandit est. Nulla facilisi. Phasellus cursus sollicitudin neque, vitae varius massa lobortis ut. Morbi blandit sit amet lectus non fermentum. Vestibulum rutrum luctus consectetur. Ut congue, dolor ut tincidunt tincidunt, lorem erat condimentum est, euismod finibus lorem velit ac magna.
+        {"\n"}
+        Maecenas malesuada, arcu nec dictum scelerisque, justo leo malesuada ligula, eu suscipit massa lectus a ligula. Nam vel sapien egestas, accumsan nisl dignissim, interdum enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris id lectus id urna ullamcorper cursus id at erat. Nam est sapien, suscipit vel augue eu, pharetra tempor felis. Donec eget sem a nibh viverra iaculis. Proin vel augue commodo, porttitor massa sit amet, pretium ipsum. Ut id mi et dolor luctus faucibus. Donec porta, nisi sed scelerisque maximus, urna nunc elementum massa, vel bibendum felis nunc a lacus. Ut molestie porta diam eget volutpat.
+        {"\n"}
+        Vestibulum cursus ex eu ipsum laoreet sagittis. Integer eget feugiat sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas placerat turpis et urna maximus, quis porttitor enim pharetra. Integer mollis sapien sit amet velit bibendum, sed faucibus massa rutrum. Pellentesque malesuada dui id tortor pharetra, ut hendrerit massa auctor. Nam ultrices, est vel venenatis iaculis, lectus sapien volutpat dui, a condimentum est quam vel ante. Nam pretium aliquam nibh et interdum. Mauris neque ante, auctor vitae dictum nec, blandit eu orci. Quisque luctus turpis et risus tincidunt eleifend. Nam non nibh id ipsum aliquet finibus in sit amet sem. Etiam varius quis ipsum a aliquam. Nam ac nisi venenatis, imperdiet neque vel, dapibus nisi. Donec aliquet, felis quis dapibus dapibus, dui erat vulputate augue, ac convallis libero purus vitae nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla faucibus, sem nec eleifend viverra, tellus velit dignissim ipsum, at ultricies risus tellus nec ex.
+        </Text>
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
       </View>
     );
   }
@@ -140,11 +143,23 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     paddingHorizontal: 4,
   },
+  getStartedTitle: {
+      fontSize: 20,
+      color: 'rgba(96,100,109, 1)',
+      lineHeight: 24,
+      textAlign: 'center',
+  },
   getStartedText: {
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
     textAlign: 'center',
+  },
+  getStartedStuff: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'left',
   },
   tabBarInfoContainer: {
     position: 'absolute',
