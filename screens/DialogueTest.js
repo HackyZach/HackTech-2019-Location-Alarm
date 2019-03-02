13a83da6433dc7cef17c98d27b84f656af141254
Nate R <nate.ruppert@csu.fullerton.edu>
2019-03-02 12:25:08 -0800

    working dual messagebox system

--- screens/DialogueTest.js
+++ screens/DialogueTest.js
@@ -6,31 +6,61 @@ import Dialog from 'react-native-dialog';
 
 export default class DialogTester extends Component {
   state = {
+    visibleBox: true,
     dialogOne: true,
+    dialogTwo: false,
   };
 
   showDialog = () => {
-    this.setState({ dialogOne: true });
+    this.setState({ dialogOne: true, dialogTwo: false, visibleBox: true });
   };
 
+  showDialogTwo = () => {
+    this.setState({ dialogTwo: true, dialogOne: false, visibleBox: true });
+  }
+
+  //TODO
   handleCancel = () => {
-    this.setState({ dialogOne: false });
+    this.setState({ visibleBox: false });
   };
 
+  //TODO
   handleDelete = () => {
-    // The user has pressed the "Delete" button, so here you can do your own logic.
-    // ...Your logic
-    this.setState({ dialogOne: false });
+    this.setState({ visibleBox: false });
   };
 
   render() {
-    const { dialogOne } = this.state;
+    const { dialogOne: dialogBox } = this.state;
+    const { dialogTwo: dialogBoxTwo } = this.state;
+    const { visibleBox: visible } = this.state;
+    console.log(dialogBox);
+    console.log(this.state.dialogOne)
+
+    console.log(dialogBox, visible)
     return (
       <View>
         <TouchableOpacity onPress={this.showDialog}>
-          <Text>Show Dialog</Text>
+          <Text>Show Dialog One</Text>
         </TouchableOpacity>
-        <Dialog.Container visible={dialogOne}>
+        <TouchableOpacity onPress={this.showDialogTwo}>
+          <Text>Show Dialog Two</Text>
+        </TouchableOpacity>
+
+        <Dialog.Container visible={dialogBox && visible}>
+          <Dialog.Title>[Dev] Messagebox Title</Dialog.Title>
+          <Dialog.Description>
+            Messagebox Description
+          </Dialog.Description>
+          <View>
+            <Dialog.Input label="Input Label" />
+            <Text> Inline text in messagebox </Text>
+            <Dialog.Input label="Input Label Two" />
+          </View>
+          <Dialog.Button label="No" onPress={this.handleCancel} />
+          <Dialog.Button label="Yes" onPress={this.handleDelete} />
+        </Dialog.Container>
+
+        <Dialog.Container visible={dialogBoxTwo && visible}>
           <Dialog.Title>Go commit heart stop?</Dialog.Title>
           <Dialog.Description>
             You will die immediately by this action.
