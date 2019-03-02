131eeba72d98d4e45cc0492c72fe1facb681afa3
Nate R <nate.ruppert@csu.fullerton.edu>
2019-03-02 03:05:59 -0800

    preliminary addition of map markers

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -8,7 +8,27 @@ export default class App extends React.Component {
     title: 'Map Screen Test',
   };
 
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markers: []
+    }
+    this.handlePress = this.handlePress.bind(this);
+  }
+
+  handlePress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+        }
+      ]
+    })
+  }
+
+
   render() {
     return (
 
@@ -19,14 +39,20 @@ export default class App extends React.Component {
     showsMyLocationButton={true}
     showsUserLocation={true}
     mapType="standard"
-
-    region={{
+    onPress={this.handlePress}
+    initialRegion={{
       latitude: 40.76727216,
       longitude: -73.99392888,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     }}
 >
+{this.state.markers.map((marker) => {
+        return (
+          <MapView.Marker {...marker} >
+          </MapView.Marker>
+        )
+      })}
 </MapView>
     );
   }
