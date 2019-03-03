0e60d8cf4f7b4989f0c2fe5d439bd80bf69e85f6
Zachary Owen <zach.owen50@gmail.com>
2019-03-02 17:58:34 -0800

    Stored necessary data for calculations.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import { MapView } from 'expo';
+import { Alert, Button, View } from 'react-native';
+
+// class alarm_trigger {
+//   constructor(user_longitude, user_latitude, marker_longitude, marker_latitude) {
+//     this.user_longitude = user_longitude;
+//     this.user_latitude = user_latitude;
+//     this.marker_longitude = marker_longitude;
+//     this.marker_latitude = marker_latitude;
+//   }
+// }
+
+let alarm_trigger = {
+  user_longitude: 0,
+  user_latitude: 0,
+  marker_longitude: 0,
+  marker_latitude: 0
+};
 
 export default class App extends React.Component {
   static navigationOptions = {
@@ -27,17 +44,25 @@ export default class App extends React.Component {
     });
   }
 
+  compute_alarm(latitude, longitude) {
+    Alert.alert(latitude, longitude);
+  }
+
   render() {
     return (
+    <View style={{flex: 1}}>
       <MapView
         style={{ flex: 1 }}
         provider="google"
         showsTraffic
         showsMyLocationButton
-        showsUserLocation
+        showsUserLocation = {true}
+        onUserLocationChange= {(event)=>{
+          alarm_trigger.user_latitude = event.nativeEvent.coordinate.latitude;
+          alarm_trigger.user_longitude = event.nativeEvent.coordinate.longitude;
+        }}
         mapType="standard"
         onLongPress={this.handlePress}
-        // onMarkerPress={}
         initialRegion={{
           latitude: 40.76727216,
           longitude: -73.99392888,
@@ -53,9 +78,12 @@ export default class App extends React.Component {
             description="Tap to Change Info"
             onCalloutPress={this.handleDialogueBox}
           >
+          alarm_trigger.marker_longitude = marker.coordinate.longitude;
+          alarm_trigger.marker_latitude = marker.coordinate.latitude;
           </MapView.Marker>
         ))}
       </MapView>
+    </View>
     );
   }
 }
