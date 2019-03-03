aeab4dfb308b13658ccdbcd72e064d113d9add4b
Nate R <nate.ruppert@csu.fullerton.edu>
2019-03-03 02:01:51 -0800

    clean and redo of much of mapscreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import { MapView } from 'expo';
 import {
-  Image,
-  Platform,
-  ScrollView,
   StyleSheet,
   Text,
   View,
-  TextInput,
 } from 'react-native';
-import { SearchBar } from 'react-native-elements';
 import haversine from 'haversine';
 import Dialog from 'react-native-dialog';
+
 
 export default class App extends React.Component {
   static navigationOptions = {
     title: 'Dev-MapTest',
   };
 
-  state = {
-    searchNew: '',
-    markers: [],
-    count: 1,
+  tempValues = {
     lat: 0,
     long: 0,
     p_lat: -1,
+  };
+
+  state = {
+    markers: [],
+    count: 1,
     close: false,
-    map: -1,
   };
 
   constructor(props) {
@@ -34,25 +31,24 @@ export default class App extends React.Component {
     this.handlePress = this.handlePress.bind(this);
   }
 
-  updateSearch = (search) => {
-    this.setState({ searchNew: search });
+  closeDiag= () => {
+    this.setState({ close: false });
   };
 
-  handleLocation = (e) => {
-    if (this.state.lat === e.nativeEvent.coordinate.latitude) {
-      return;
+  markerName(e) {
+    return `Map Marker #${e.key}`;
+  }
+
+  isClose(e, mark) {
+    if (e <= 250) {
+      console.log(`YAY! CLOSE TO MAP MARKER ${mark.key}`);
+      this.setState({ close: true });
+      return true;
     }
-    // User coordinates
-    // console.log(e.nativeEvent.coordinate.latitude, e.nativeEvent.coordinate.longitude)
-    this.setState({
-      lat: e.nativeEvent.coordinate.latitude,
-      long: e.nativeEvent.coordinate.longitude,
-      p_lat: this.state.lat,
-    });
-  };
+    return false;
+  }
 
   handlePress(e) {
-    this.setState({ count: this.state.count + 1 });
     this.setState({
       markers: [
         ...this.state.markers,
@@ -64,119 +60,101 @@ export default class App extends React.Component {
     });
   }
 
-  isClose(e, mark) {
-    if (e <= 250) {
-      console.log(`YAY! CLOSE TO MAP MARKER ${mark.key}`);
-      this.setState({ close: true });
-      this.setState({ map: mark.key });
-      return true;
-    }
-    return false;
+handleLocation = (e) => {
+  const userLat = e.nativeEvent.coordinate.latitude;
+  const tempLat = this.tempValues.lat;
+  // console.log(userLat);
+  // console.log(tempLat);
+  if (tempLat === userLat) {
+    return;
   }
+  console.log('[Debug] Location Change Detected');
+  // User coordinates
+  // console.log(e.nativeEvent.coordinate.latitude, e.nativeEvent.coordinate.longitude)
+  this.tempValues.lat = userLat;
+  this.tempValues.long = e.nativeEvent.coordinate.longitude;
+  this.tempValues.p_lat = tempLat;
+};
 
-  markerName(e) {
-    return `Map Marker #${e.key}`;
+calculateDist(e) {
+  const tempLat = this.tempValues.lat;
+  const tempLong = this.tempValues.long;
+  if (tempLat === this.tempValues.p_lat) {
+    return;
   }
+  const start = {
+    latitude: tempLat,
+    longitude: tempLong,
+  };
+  this.tempValues.p_lat = tempLat;
+  const end = {
+    latitude: e.coordinate.latitude,
+    longitude: e.coordinate.longitude,
+  };
+  // console.log(haversine(start, end));
+  const value = haversine(start, end, { unit: 'meter' });
+  console.log(`Lat: ${tempLat} Long: ${tempLong} Dist: ${value}`);
+  return value;
+}
 
-  handleDialogueBox(e) {
-    // Not working
-    // console.log(e);
-  }
-
-  calculateDist(e) {
-    if (this.state.lat === this.state.p_lat) {
-      return;
-    }
-    const start = {
-      latitude: this.state.lat,
-      longitude: this.state.long,
-    };
-    this.setState({
-      p_lat: this.state.lat,
-    });
-    const end = {
-      latitude: e.coordinate.latitude,
-      longitude: e.coordinate.longitude,
-    };
-    // console.log(haversine(start, end));
-    const value = haversine(start, end, { unit: 'meter' });
-    console.log(`Lat: ${this.state.lat} Long: ${this.state.long} Dist: ${value}`);
-    this.setState({ dist: value });
-    return value;
-  }
-
-  close= () => {
-    this.setState({ close: false });
-  }
-
-  render() {
-    const { searchNew: search } = this.state;
-    // User Coords
-    const { lat: lati } = this.state;
-    const { long: longi } = this.state;
-    // console.log(lati, longi)
-    const { i: v } = this.state;
-    const { map: marker } = this.state;
-    console.log(search);
-
-    return (
-      <MapView
-        style={{ flex: 1 }}
-        provider="google"
-        showsTraffic
-        showsMyLocationButton
-        showsUserLocation
-        mapType="standard"
-        onLongPress={this.handlePress}
-        onUserLocationChange={this.handleLocation}
+render() {
+  // Possible broken map markers - considering it a feature for now, dialogue box shows only once
+  return (
+    <MapView
+      style={{ flex: 1 }}
+      provider="google"
+      showsTraffic
+      showsMyLocationButton
+      showsUserLocation
+      mapType="standard"
+      onLongPress={this.handlePress}
+      onUserLocationChange={this.handleLocation}
         // onMarkerPress={}
-        initialRegion={{
-          latitude: 34.1401239,
-          longitude: -118.1250156,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <MapView.Callout>
-          <View>
-            <Text style={styles.calloutView}> Yay! Im a callout! </Text>
+      initialRegion={{
+        latitude: 34.1401239,
+        longitude: -118.1250156,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    >
+      <MapView.Callout>
+        <View>
+          <Text style={styles.calloutView}> Yay! Im a callout! </Text>
 
-            <Dialog.Container visible={this.state.close}>
-              <Dialog.Title>Hey! Listen!</Dialog.Title>
-              <Dialog.Description>
+          <Dialog.Container visible={this.state.close}>
+            <Dialog.Title>Hey! Listen!</Dialog.Title>
+            <Dialog.Description>
               You are really close to your destination!
-              </Dialog.Description>
-              <Dialog.Button label="Nice!" onPress={this.close} />
-            </Dialog.Container>
-          </View>
-        </MapView.Callout>
+            </Dialog.Description>
+            <Dialog.Button label="Nice!" onPress={this.closeDiag} />
+          </Dialog.Container>
+        </View>
+      </MapView.Callout>
 
-        {this.state.markers.map(marker => (
-          // console.log(marker),
-          <View key={marker.coordinate.latitude}>
-            <MapView.Marker
-              {...marker}
-              onDrag={this.updateCircle}
-              title={this.markerName(marker)}
-              description="Tap to Change Info"
-              distance={this.calculateDist(marker)}
-              close={this.isClose(this.calculateDist(marker), marker)}
-              onCalloutPress={this.handleDialogueBox(marker)}
-            >
-            </MapView.Marker>
-            <MapView.Circle
-              center={{
-                latitude: marker.coordinate.latitude,
-                longitude: marker.coordinate.longitude,
-              }}
-              radius={250}
-            >
-            </MapView.Circle>
-          </View>
-        ))}
-      </MapView>
-    );
-  }
+      {this.state.markers.map(marker => (
+        // console.log(marker),
+        <View key={marker.coordinate.latitude}>
+          <MapView.Marker
+            {...marker}
+            onDrag={this.updateCircle}
+            title={this.markerName(marker)}
+            description="Tap to Change Info"
+          >
+            {this.isClose(this.calculateDist(marker), marker)}
+          </MapView.Marker>
+          <MapView.Circle
+            center={{
+              latitude: marker.coordinate.latitude,
+              longitude: marker.coordinate.longitude,
+            }}
+            radius={250}
+          >
+          </MapView.Circle>
+        </View>
+      ))}
+    </MapView>
+  );
+}
 }
 
 const styles = StyleSheet.create({
